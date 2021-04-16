2a2cab9674d6d4e6902152c5ea0f1f1e610b87a7
Joon Park <hyojoon2372552@naver.com>
2021-04-16 11:21:08 +0900

    make attach_node_to_list function

--- linked_list.c
+++ linked_list.c
@@ -4,17 +4,44 @@
 
 typedef struct node {
 
-    bool isHead; 
+    // bool isHead; 
     int val;
     struct node* next;
 
 } Node;
 
+Node* create_node(int val) {
+
+    Node* new_node = (Node*) malloc (sizeof(Node));
+
+    new_node->val = val;
+    new_node->next = NULL;
+
+    return new_node;
+}
+
+void attach_node_to_list(Node* head, Node* node) {
+    
+    Node* cursor = head;
+
+    while(cursor->next != NULL) {
+        cursor = cursor->next;
+    }
+
+    cursor->next = node;
+}
+
 
 int main() {
 
-    Node* test_list = create_empty_list();
-    
+    Node* head = create_node(0);
+    Node* second = create_node(1);
+
+    attach_node_to_list(head,second);
+
+
+    printf("the val: %d\n", head->val);
+    printf("the val: %d\n", head->next->val);
 
 
     return 0;
