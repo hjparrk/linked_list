bde21ffd80dbd487e4257151e28dbb2018958b84
Joon Park <hyojoon2372552@naver.com>
2021-04-16 11:03:35 +0900

    make create_empty_list() function

--- linked_list.c
+++ linked_list.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
-struct node {
-    
-    bool isHead;
-    int key;
+typedef struct node {
+
+    bool isHead; 
     int val;
-    struct node *next;
+    struct node* next;
+
+} Node;
+
+Node* create_empty_list() {
+
+    Node* head =  (Node*)malloc(sizeof(Node));
+    head->isHead = true;
+    head->val =  -1;
+    head->next = NULL;
+
+    return head;
 }
 
 int main() {
 
+    Node* test_list = create_empty_list();
+    
 
 
     return 0;
