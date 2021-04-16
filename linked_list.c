a82ae59f7d5c9d9dbee7d7de813ef9e20b2267f1
Joon Park <hyojoon2372552@naver.com>
2021-04-16 12:12:33 +0900

    make print_list function

--- linked_list.c
+++ linked_list.c
@@ -1,35 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
+// structure of Node
 typedef struct node {
-
-    // bool isHead; 
+    
+    //value
     int val;
+    // point to the next node
     struct node* next;
 
 } Node;
 
+// function that creates a new node
+// accept integer value as parameter
 Node* create_node(int val) {
 
+    // create new node
     Node* new_node = (Node*) malloc (sizeof(Node));
 
+    // set the value and the next node as NULL
     new_node->val = val;
     new_node->next = NULL;
 
+    // return the newly created node
     return new_node;
 }
 
+// function that attach a new node to linked_list
+// accept head node and new node to attach as parameters
 void attach_node_to_list(Node* head, Node* node) {
     
+    // assign a cursor to the head node
     Node* cursor = head;
 
+    //traverse the list and the find the last node
     while(cursor->next != NULL) {
         cursor = cursor->next;
     }
 
+    // assign a new node to last node's next
     cursor->next = node;
 }
+
+// print all data starting from the head node;
+void print_list (Node* head) {
+
+    // assign the cursor to the head node
+    Node* cursor = head;
+
+    printf("The data : ");
+
+    // traverse the list
+    while(cursor->next != NULL) {
+        // print the current node's data
+        printf("%d\t",cursor->val);
+        // iterate to the next node
+        cursor = cursor->next;
+    }
+    // print the last node of the list
+    printf("%d\n",cursor->val);
+} 
 
 
 int main() {
@@ -39,9 +69,10 @@ int main() {
 
     attach_node_to_list(head,second);
 
+    print_list(head);
 
-    printf("the val: %d\n", head->val);
-    printf("the val: %d\n", head->next->val);
+    // printf("the val: %d\n", head->val);
+    // printf("the val: %d\n", head->next->val);
 
 
     return 0;
